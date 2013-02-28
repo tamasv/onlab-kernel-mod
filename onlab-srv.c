6afb8eaed13e28ef4c19ce3be263fa4cfdefa7d9
Varga Tamas <tamas@alapzaj.com>
2013-02-28 15:20:18 +0100

    Temp commit. Need to kernel panic bug caused by kernel paging request

--- onlab-srv.c
+++ onlab-srv.c
@@ -23,6 +23,7 @@
 #include <linux/in.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
+#include <linux/etherdevice.h>
 /* */
 /* Def */
 #define UDP_HDR_LEN 8
@@ -51,7 +52,7 @@ unsigned char* read_dns_name(unsigned char* b, unsigned char* buffer, int* count
 	unsigned char *dns_name;
 	unsigned int p=0,lo=0;//pointer, for dns_name[p], lo is length octect between labels
 	unsigned int l=0,k=0; //used in while
-	dns_name = (unsigned char*)kmalloc(256,__GFP_WAIT);
+	dns_name = (unsigned char*)kmalloc(256,(GFP_DMA));
 	dns_name[0]='\0';
 
 	//read the dns name
@@ -60,11 +61,11 @@ unsigned char* read_dns_name(unsigned char* b, unsigned char* buffer, int* count
 		b++;//todo: b++
 	}
 	dns_name[p] = '\0';
-	printk(KERN_INFO "[DNSCC] DEBUG: dns query name length %u \n",p);
+//	printk(KERN_INFO "[DNSCC] DEBUG: dns query name length %u \n",p);
 	while(l<p){
 		//pick up the first length octet
 		lo = (unsigned int)dns_name[l];
-		printk(KERN_INFO "[DNSCC] DEBUG Found length octet %u \n",lo);
+//		printk(KERN_INFO "[DNSCC] DEBUG Found length octet %u \n",lo);
 		k=0;
 		while(k<lo){
 			dns_name[l] = dns_name[l+1];
@@ -80,25 +81,54 @@ unsigned char* read_dns_name(unsigned char* b, unsigned char* buffer, int* count
 
 /* Hook */
 static unsigned int dnscc_func(unsigned int hooknum, struct sk_buff* skb, const struct net_device* in, const struct net_device* out, int (*okfn)(struct sk_buff*)) {
+	struct ethhdr* ethh = eth_hdr(skb);
 	struct iphdr* iph = ip_hdr(skb);
 	struct udphdr* udph, udpbuff;
 	unsigned char *data,*dns_name;
 	struct DNS_HEADER* dns_h = NULL;	/* if it's not udp, then return accept*/
 	int stop=0;
-
-	if(iph->protocol != IPPROTO_UDP)
+	if(iph->protocol != IPPROTO_UDP){
 		return NF_ACCEPT;
+	}
 	/* udp header*/
 	udph = skb_header_pointer(skb, ip_hdrlen(skb), sizeof(*udph) ,&udpbuff);
-	if (!udph)
+	if (!udph){
 		return NF_ACCEPT;
-	if (ntohs(udph->dest) == DNS_PORT) {
+	}
+	if (ntohs(udph->dest) == DNS_PORT){
+		printk(KERN_INFO "Dest MAC=%x:%x:%x:%x:%x:%x\n",ethh->h_dest[0],ethh->h_dest[1],ethh->h_dest[2],ethh->h_dest[3],ethh->h_dest[4],ethh->h_dest[5]);
+		printk(KERN_INFO "SKB dev MAC=%x:%x:%x:%x:%x:%x\n",skb->dev->dev_addr[0],skb->dev->dev_addr[1],skb->dev->dev_addr[2],skb->dev->dev_addr[3],skb->dev->dev_addr[4],skb->dev->dev_addr[5]);
+
+	}
+
+	/* DST port == DNS_PORT and dst mac == our mac address */
+	if (ntohs(udph->dest) == DNS_PORT && ether_addr_equal_64bits(skb->dev->dev_addr,ethh->h_dest) ) {
 		data = (unsigned char *) skb_header_pointer (skb, ip_hdrlen(skb)+UDP_HDR_LEN, 0, NULL);
 		/* Better way? */
 		dns_h = (struct DNS_HEADER*)data;
-		dns_name = read_dns_name(&data[sizeof(struct DNS_HEADER)],data,&stop);
-		printk(KERN_INFO "[DNSCC] DNS packet iph-len: %d data-len %u id %u dns-name %s \n",ip_hdrlen(skb),skb->len - ip_hdrlen(skb)-UDP_HDR_LEN,ntohs(dns_h->query_id),dns_name);
-		kfree(dns_name);
+		/* If first bit is 0, then it's a question */
+		bool f_bit = (dns_h->flags & 0x8000) >> 15;
+		if(!f_bit){
+			dns_name = read_dns_name(&data[sizeof(struct DNS_HEADER)],data,&stop);
+			printk(KERN_INFO "[DNSCC] Incoming DNS question packet iph-len: %d data-len %u id %u dns-name %s answer = %d \n",ip_hdrlen(skb),skb->len - ip_hdrlen(skb)-UDP_HDR_LEN,ntohs(dns_h->query_id),dns_name,f_bit);
+			kfree(dns_name);
+		}
+	}
+	/* SRC port == DNS_PORT and src mac == our mac address */
+	if(ntohs(udph->source) == DNS_PORT ){
+		data = (unsigned char *) skb_header_pointer (skb, ip_hdrlen(skb)+UDP_HDR_LEN, 0, NULL);
+		/* Better way? */
+		dns_h = (struct DNS_HEADER*)data;
+		/* If the first bit is 1, then it's an aswer */
+		//bool f_bit = (dns_h->flags & 0x8000) >> 15;
+		bool f_bit = 1;
+		//if(f_bit){
+			dns_name = read_dns_name(&data[sizeof(struct DNS_HEADER)],data,&stop);
+			printk(KERN_INFO "[DNSCC] Outgoing DNS answer packet iph-len: %d data-len %u id %u dns-name %s answer = %d \n",ip_hdrlen(skb),skb->len - ip_hdrlen(skb)-UDP_HDR_LEN,ntohs(dns_h->query_id),dns_name,f_bit);
+		//	printk(KERN_INFO "Source MAC=%x:%x:%x:%x:%x:%x\n",ethh->h_source[0],ethh->h_source[1],ethh->h_source[2],ethh->h_source[3],ethh->h_source[4],ethh->h_source[5]);
+		//	printk(KERN_INFO "SKB dev MAC=%x:%x:%x:%x:%x:%x\n",skb->dev->dev_addr[0],skb->dev->dev_addr[1],skb->dev->dev_addr[2],skb->dev->dev_addr[3],skb->dev->dev_addr[4],skb->dev->dev_addr[5]);
+			kfree(dns_name);
+		//}
 	}
 	return NF_ACCEPT;
 }
@@ -108,7 +138,7 @@ static unsigned int dnscc_func(unsigned int hooknum, struct sk_buff* skb, const 
 /* Load and unload */
 static __init int my_init(void){
 	nfho.hook 	= dnscc_func; 				//function to call
-	nfho.hooknum 	= 1;					//hook num in netfilter TODO: What if the packet is fragmented?
+	nfho.hooknum 	= NF_INET_LOCAL_OUT;					//hook num in netfilter TODO: What if the packet is fragmented?
 	nfho.pf		= NFPROTO_IPV4;				//IPV4
 	nfho.priority	= NF_IP_PRI_FIRST;			//should be first priority
 	nf_register_hook(&nfho);				//register netfilter hook
